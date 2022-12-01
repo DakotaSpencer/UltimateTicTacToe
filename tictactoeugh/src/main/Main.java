7b23cd85f221a1cc9bd8bf6db675626c000d8de4
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2022-12-01 11:01:01 -0700

    Implemented Main Menu and Computer VS Computer

--- tictactoeugh/src/main/Main.java
+++ tictactoeugh/src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -57,6 +58,10 @@ public class Main {
     public static void mainMenu(Player @NotNull [] players) {
         System.out.println("Welcome to Tic Tac Toe.");
 
+        Random rng = new Random();
+        int num1 = rng.nextInt(10) + 1;
+        System.out.println(num1);
+
         final String ANSI_BLACK = "\u001B[30m";
         final String ANSI_RED = "\u001B[31m";
         final String ANSI_GREEN = "\u001B[32m";
@@ -66,51 +71,73 @@ public class Main {
         final String ANSI_CYAN = "\u001B[36m";
         final String ANSI_WHITE = "\u001B[37m";
 
-        for(int i = 0; i < players.length; i++) {
-            boolean finished = false;
-            while (!finished) {
-                System.out.println("Player " + (i+1) + ": Choose your colour.");
-                System.out.println("1 - " + ANSI_BLACK + "Black" + ANSI_RESET);
-                System.out.println("2 - " + ANSI_RED + "Red" + ANSI_RESET);
-                System.out.println("3 - " + ANSI_GREEN + "Green" + ANSI_RESET);
-                System.out.println("4 - " + ANSI_YELLOW + "Yellow" + ANSI_RESET);
-                System.out.println("5 - " + ANSI_BLUE + "Blue" + ANSI_RESET);
-                System.out.println("6 - " + ANSI_PURPLE + "Purple" + ANSI_RESET);
-                System.out.println("7 - " + ANSI_CYAN + "Cyan" + ANSI_RESET);
-                System.out.println("8 - " + ANSI_WHITE + "White" + ANSI_RESET);
+        System.out.println("Choose a game mode.");
+        System.out.println("1 - Player VS Player");
+        System.out.println("2 - Player VS Computer");
+        System.out.println("3 - Computer VS Computer");
+        Scanner scannyboi = new Scanner(System.in);
+        System.out.println("Mode:");
+        int modeinput = scannyboi.nextInt();
+        if(modeinput > 0 && modeinput < 4) {
+            switch (modeinput) {
+                case 1:
+                    for(int i = 0; i < players.length; i++) {
+                        boolean finished = false;
+                        while (!finished) {
+                            System.out.println("Player " + (i+1) + ": Choose your colour.");
+                            System.out.println("1 - " + ANSI_BLACK + "Black" + ANSI_RESET);
+                            System.out.println("2 - " + ANSI_RED + "Red" + ANSI_RESET);
+                            System.out.println("3 - " + ANSI_GREEN + "Green" + ANSI_RESET);
+                            System.out.println("4 - " + ANSI_YELLOW + "Yellow" + ANSI_RESET);
+                            System.out.println("5 - " + ANSI_BLUE + "Blue" + ANSI_RESET);
+                            System.out.println("6 - " + ANSI_PURPLE + "Purple" + ANSI_RESET);
+                            System.out.println("7 - " + ANSI_CYAN + "Cyan" + ANSI_RESET);
+                            System.out.println("8 - " + ANSI_WHITE + "White" + ANSI_RESET);
 
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Color:");
-                int input = scanner.nextInt();
-                if(input > 0 && input < 9) {
-                    switch(input) {
-                        case 1:
-                            players[i].setColour(ANSI_BLACK);
-                            break;
-                        case 2:
-                            players[i].setColour(ANSI_RED);
-                            break;
-                        case 3:
-                            players[i].setColour(ANSI_GREEN);
-                            break;
-                        case 4:
-                            players[i].setColour(ANSI_YELLOW);
-                            break;
-                        case 5:
-                            players[i].setColour(ANSI_BLUE);
-                            break;
-                        case 6:
-                            players[i].setColour(ANSI_PURPLE);
-                            break;
-                        case 7:
-                            players[i].setColour(ANSI_CYAN);
-                            break;
-                        case 8:
-                            players[i].setColour(ANSI_WHITE);
-                            break;
+                            Scanner scanner = new Scanner(System.in);
+                            System.out.println("Color:");
+                            int input = scanner.nextInt();
+                            if(input > 0 && input < 9) {
+                                switch(input) {
+                                    case 1:
+                                        players[i].setColour(ANSI_BLACK);
+                                        break;
+                                    case 2:
+                                        players[i].setColour(ANSI_RED);
+                                        break;
+                                    case 3:
+                                        players[i].setColour(ANSI_GREEN);
+                                        break;
+                                    case 4:
+                                        players[i].setColour(ANSI_YELLOW);
+                                        break;
+                                    case 5:
+                                        players[i].setColour(ANSI_BLUE);
+                                        break;
+                                    case 6:
+                                        players[i].setColour(ANSI_PURPLE);
+                                        break;
+                                    case 7:
+                                        players[i].setColour(ANSI_CYAN);
+                                        break;
+                                    case 8:
+                                        players[i].setColour(ANSI_WHITE);
+                                        break;
+                                }
+                                finished = true;
+                            }
+                        }
                     }
-                    finished = true;
-                }
+                    break;
+                case 2:
+                    System.out.println("Not Yet Implemented");
+                    mainMenu(players);
+                    break;
+                case 3:
+                    PlayerVComputer.main();
+                    System.out.println("Game is over! Exiting app...");
+                    System.exit(1);
+                    break;
             }
         }
     }
